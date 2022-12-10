182a85641af0896d3e928b08e4eab7b58122b030
HA-SEUNG-JEONG <gktmd652@naver.com>
2022-12-10 13:19:07 +0900

    feat : 홀수 n개 더하기(명령형)

---
+++ index.js
@@ -0,0 +1,13 @@
+function f1(limit, list) {
+  let acc = 0;
+  for (const item of list) {
+    if (item % 2) {
+      const square = item * item;
+      acc += square;
+      if (--limit === 0) break;
+    }
+  }
+  console.log(acc);
+}
+
+f1(3, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
