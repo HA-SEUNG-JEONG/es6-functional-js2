1df8d8899eb6f92d29f5e697bb4f8e3b3b685cfc
HA-SEUNG-JEONG <gktmd652@naver.com>
2023-01-18 10:51:41 +0900

    축약 및 합산을 reduce로

--- index.js
+++ index.js
@@ -15,19 +15,42 @@ function f1(limit, list) {
 
 const { L, C } = window._;
 
+const add = (acc, cur) => acc + cur;
+
 function f2(limit, list) {
-  let acc = 0;
-  for (const a of L.take(
-    limit,
-    L.map(
-      (a) => a * a,
-      L.filter((a) => a % 2, list)
+  // [...,,,]로 즉시 평가
+  // console.log([
+  //   ...L.take(
+  //     limit,
+  //     L.map(
+  //       (a) => a * a,
+  //       L.filter((a) => a % 2, list)
+  //     )
+  //   ),
+  // ]);
+  console.log(
+    _.reduce(
+      add,
+      0,
+      L.take(
+        limit,
+        L.map(
+          (a) => a * a,
+          L.filter((a) => a % 2, list)
+        )
+      )
     )
-  )) {
-    //f1 함수에서는 모든 list를 다 돌지만 f2 함수에서는 L.filter로 홀수값만 순회한다.
-    acc += a;
-  }
-  console.log(acc);
+  );
+
+  //_.go로 작성
+  _.go(
+    list,
+    L.filter((a) => a % 2),
+    L.map((a) => a * a),
+    L.take(limit),
+    _.reduce(add),
+    console.log
+  );
 }
 
 f2(3, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
